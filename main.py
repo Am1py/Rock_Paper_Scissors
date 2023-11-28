c50c99ad01d6967f2d41eb604309eacebc68d75a
Am1py <pawel.omiecinski@gmail.com>
2023-11-28 19:44:45 +0100

    Update main.py
    Adding two functions, computer selection, and determining the winner.

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def player_choice():
         except ValueError:
             print("Inalid input. PLease enter a valid string.")
 
+def computer_choise ():
+    computer = ['rock','paper','scissors']
+    return random.choice(computer)
+
+def winner_decider(computer,user):
+    if user == computer:
+        return ("\nIt's tie!")
+    elif (user == 'rock' and computer == 'scissors') or (user == 'scissors' and computer == 'paper') or (user == 'paper' and computer == 'rock'):
+        return ('\nYou win!')
+    else:
+        print('\nComputer win!')
+
+
 
 
 
